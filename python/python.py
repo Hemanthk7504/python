e74f0de261afff5f95aa16f5fb11b64aae97cdcf
hemanthk7504 <hemanthk.b@proton.me>
2023-03-16 09:36:42 +0530

    Add project files.

---
+++ python/python.py
@@ -0,0 +1,12 @@
+import pandas
+def main():
+    Employe_name = input("Enter Employe details: ")
+    Employe_Id = int(input("Enter Employe Id : "))
+    Employe_salary = float(input("Enter salary details : "))
+    Data = pandas.DataFrame({
+        "Employe Name":[Employe_name],
+        "Employe Id":[Employe_Id],
+        "Employe salary":[Employe_salary]
+    })
+
+    print(Data)
