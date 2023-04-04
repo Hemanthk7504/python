003f90b2cdc3d66affe7b23ad664a71113bbdef3
Hemanthk7504 <127845472+Hemanthk7504@users.noreply.github.com>
2023-04-04 12:28:54 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #pandas
 #edited
-import pandas
+#import pandas
 #FFWF
 
 
@@ -8,10 +8,7 @@ def main():
     Employe_name = input("Enter Employe details: ")
     Employe_Id = int(input("Enter Employe Id : "))
     Employe_salary = float(input("Enter salary details : "))
-    Data = pandas.DataFrame({
-        "Employe Name":[Employe_name],
-        "Employe Id":[Employe_Id],
-        "Employe salary":[Employe_salary]
-    })
+    print(Employe_name)
 
     print(Data)
+main()
